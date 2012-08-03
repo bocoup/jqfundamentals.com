67a6d62ffdd9c79acda3a005aa7bda39b30f1648
Rebecca Murphey <rmurphey@gmail.com>
2012-08-03 13:52:49 -0400

    wip on exercises

--- server/index.js
+++ server/index.js
@@ -130,19 +130,23 @@ app.get('/sandbox/:name', function(req, res) {
   });
 });
 
-app.get('/exercises/:exercise', function(req, res) {
-  var exerciseMarkdown = [ exerciseDir, req.params.exercise, 'index.md' ].join('/');
-  render( exerciseMarkdown, 'exercise', res);
-});
-
-app.get('/exercises/:name/index.html', function(req, res) {
-  var file = [ exerciseDir, req.params.name, 'index.html' ].join('/');
+app.get('/exercises/:exercise/index.html', function(req, res) {
+  var file = [ exerciseDir, req.params.exercise, 'index.html' ].join('/');
 
   fs.readFile(file, function(err, data) {
     res.render('iframe', {
       content: data
     });
   });
+});
+
+app.get('/exercises/:exercise/:step', function(req, res) {
+  var exerciseMarkdown = [
+    exerciseDir,
+    req.params.exercise,
+    req.params.step + '.md'
+  ].join('/');
+  render( exerciseMarkdown, 'exercise', res);
 });
 
 app.get('/chapter/:name', function(req, res) {
