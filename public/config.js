d0aa994a297661ff1e776067260c26bbd458c58e
Rebecca Murphey <rmurphey@gmail.com>
2012-08-24 17:12:05 -0400

    codemirror shim

--- public/config.js
+++ public/config.js
@@ -1,6 +1,12 @@
 require.config({
   baseUrl : '../public',
   deps : [ 'js/chapter' ],
+  shim : {
+    'vendor/codemirror' : {
+      deps : [ 'vendor/CodeMirror-2.25/lib/codemirror'],
+      exports : 'CodeMirror'
+    }
+  },
   paths : {
     jquery : 'vendor/jquery',
     widgets : 'js/widgets',
--- public/vendor/codemirror.js
+++ public/vendor/codemirror.js
@@ -1,5 +1,4 @@
 define([
-  './CodeMirror-2.25/lib/codemirror',
   './CodeMirror-2.25/mode/javascript/javascript',
   './CodeMirror-2.25/mode/xml/xml'
 ], function() {
