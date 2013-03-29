ddb1d86daf855adb96e5146df9ffd70c3e97644b
Boaz Sender <boaz@bocoup.com>
2013-03-29 15:57:37 -0400

    adding grunt as dev dep

---
+++ gruntfile.js
@@ -0,0 +1,61 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    lint: {
+      files: ['grunt.js', 'public/js/**/*.js' ]
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        CodeMirror : true,
+        define : true,
+        require : true,
+        _gaq : true
+      }
+    },
+    uglify: {},
+    requirejs: {
+      mainConfigFile : 'public/main.js',
+      dir : 'build',
+
+      modules : [
+        { name : 'js/chapter' }
+      ]
+    },
+    mocha : {
+      index : [ 'test/index.html' ]
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint');
+  grunt.loadNpmTasks('grunt-requirejs');
+  grunt.loadNpmTasks('grunt-mocha');
+
+};
